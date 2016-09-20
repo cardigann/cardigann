718402a49d3baf65faae5afe6c3ef90379efe3bc
lxt2 <rznqsp8ffyeprn2l@gmail.com>
2016-09-20 11:53:09 +1000

    Add an append filter

--- indexer/filters.go
+++ indexer/filters.go
@@ -78,6 +78,13 @@ func invokeFilter(name string, args interface{}, value string) (string, error) {
 		}
 		return strings.Trim(value, cutset), nil
 
+	case "append":
+		str, ok := args.(string)
+		if !ok {
+			return "", fmt.Errorf("Filter %q requires a string argument at idx 0", name)
+		}
+		return value + str, nil
+
 	case "timeago", "fuzzytime", "reltime":
 		return filterFuzzyTime(value, time.Now())
 	}
