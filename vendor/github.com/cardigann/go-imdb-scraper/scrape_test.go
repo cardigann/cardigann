644c3aee417b1310eadfff00432bf83299b1ee46
lxt2 <rznqsp8ffyeprn2l@gmail.com>
2017-09-21 12:57:57 +1000

    Update go-imdb-scraper

--- vendor/github.com/cardigann/go-imdb-scraper/scrape_test.go
+++ vendor/github.com/cardigann/go-imdb-scraper/scrape_test.go
@@ -2,6 +2,7 @@ package imdbscraper
 
 import (
 	"fmt"
+	"log"
 	"testing"
 )
 
@@ -14,6 +15,7 @@ func TestScrapingMovies(t *testing.T) {
 	}{
 		{"tt0087182", "Dune", "1984"},
 		{"tt1800302", "Gold", "2016"},
+		{"tt0451279", "xxWonder Woman", "2017"},
 	}
 	for _, tc := range testCases {
 		t.Run(fmt.Sprintf("%s => %s (%s)", tc.id, tc.title, tc.year), func(t *testing.T) {
@@ -21,6 +23,7 @@ func TestScrapingMovies(t *testing.T) {
 			if err != nil {
 				t.Fatal(err)
 			}
+			log.Printf("%#v", m)
 
 			if m.Title != tc.title {
 				t.Fatalf("Expected %q, got %q", tc.title, m.Title)
